46fa72e463c25ac3b2c518650452a16ebc8ca036
Shakl0ne <Shakl0ne@users.noreply.github.com>
2026-09-08 05:59:44 +0000

    deploy: 8482f37953b99692654869d8944d4a5728b2458b 8482f37953b99692654869d8944d4a5728b2458b

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.9Gb_BOMu.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.C9HdV3Fg.js";import{_ as a}from"../app.DD1v_xIX.js";import"./chunk-5VM5RSS4.B1MvD9ji.js";import"./chunk-XXDRQBXY.D4KT2LLx.js";import"./chunk-VR4S4FIN.Bf_JJgx9.js";import"./chunk-32BRIVSS.C0IFngTH.js";import"./channel.BUNfUiVC.js";import"./framework.DxG24V0C.js";import"./theme.BPhvBnU5.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
